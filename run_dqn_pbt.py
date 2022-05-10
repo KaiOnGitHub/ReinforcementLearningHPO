7e3a167f752534713d809f31f23c60e5430842ae
Kai Koenig <kai.koenig@pm.me>
2022-05-10 21:45:45 +0200

    first version of pbt implementation

--- run_dqn_pbt.py
+++ run_dqn_pbt.py
@@ -11,105 +11,68 @@ import gym
 import numpy as np
 import json
 import math
+import ray
+import torch
 from ray import tune
+from ray.tune.schedulers import PopulationBasedTraining
 
-class SaveOnBestTrainingRewardCallback(BaseCallback):
-    """
-    Callback for saving a model (the check is done every ``check_freq`` steps)
-    based on the training reward (in practice, we recommend using ``EvalCallback``).
-
-    :param check_freq:
-    :param log_dir: Path to the folder where the model will be saved.
-      It must contains the file created by the ``Monitor`` wrapper.
-    :param verbose: Verbosity level.
-    """
-    def __init__(self, check_freq: int, log_dir: str, verbose: int = 1, seed: int = 12345):
-        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
-        self.check_freq = check_freq
-        self.log_dir = log_dir
-        self.save_path = os.path.join(log_dir, 'best_model')
-        self.best_mean_reward = -np.inf
-        self.seed = seed
-
-    def _init_callback(self) -> None:
-        # Create folder if needed
-        if self.save_path is not None:
-            os.makedirs(self.save_path, exist_ok=True)
-
-    def _on_step(self) -> bool:
-        if self.n_calls % self.check_freq == 0:
-
-          # Retrieve training reward
-          x, y = ts2xy(load_results(self.log_dir), 'timesteps')
-          if len(x) > 0:
-              # Mean training reward over the last 100 episodes
-              mean_reward = np.mean(y[-100:])
-              if mean_reward > self.best_mean_reward:
-                  self.best_mean_reward = mean_reward
-
-        return True
-
-    def _on_training_end(self) -> None:
-        """
-        This event is triggered before exiting the `learn()` method.
-        """
-        # Retrieve training reward
-        x, y = ts2xy(load_results(self.log_dir), 'timesteps')
-        if len(x) > 0:
-              # Mean training reward over the last 100 episodes
-             mean_reward = np.mean(y[-100:])
-             for idx, re in enumerate(y):
-                 print("Episode %s: %s" % (idx+1, re))
-             if self.verbose > 0:
-                print(f"Num timesteps: {self.num_timesteps}")
-                print(f"Best mean reward: {self.best_mean_reward:.2f} - Last mean reward per episode: {mean_reward:.2f}")
-
-        return True
-
-
-def run_dqn(config: Dict, environment: str = 'Pong-v0'):
-    learning_rate = config.get("lr").sample()
-    gamma = config.get("gammas").sample()
-    eps = config.get("epsilons").sample()
+def run_dqn(config: Dict, checkpoint_dir=None):
+    environment = 'Pong-v0'
+    learning_rate = config.get("lr")
+    gamma = config.get("gammas")
+    eps = config.get("epsilons")
     
     seed = 67890
-    # Create log dir
-    log_dir = "tmp_DQN_%s_%s_%s_%s/" % (environment, learning_rate, gamma, eps)
-    os.makedirs(log_dir, exist_ok=True)
 
     # Create and wrap the environment
-    env = make_atari_env(environment, n_envs=1, seed=seed, monitor_dir=log_dir)
+    env = make_atari_env(environment, n_envs=1, seed=seed)
     env = VecFrameStack(env, n_stack=1)
 
     model = DQN('CnnPolicy', env, verbose=0, learning_rate=learning_rate, gamma=gamma,
                 exploration_fraction=1, exploration_initial_eps=eps, exploration_final_eps=eps, seed=seed)
-    # Create the callback: check every 1000 steps
-    callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=log_dir, seed=seed)
     rewards = []
     std_rewards = []
+
+    # If checkpoint_dir is not None, then we are resuming from a checkpoint.
+    if checkpoint_dir:
+        print("Loading from checkpoint.")
+        path = os.path.join(checkpoint_dir, "checkpoint")
+        model = DQN.load(path, env)
+        # TODO: We should probably load the timestep from the checkpoint when training was stopped and resume there.
+
     # Train the agent
-    timesteps = int(2e6/1e4)
+    timesteps = int(1e6/1e4)
     for i in range(timesteps):
-        model.learn(total_timesteps=int(1e4), callback=callback)
+        model.learn(total_timesteps=int(10e3))
         # Returns average and standard deviation of the return from the evaluation
         r, std_r = evaluate_policy(model=model, env=env)
         rewards.append(r)
         std_rewards.append(std_r)
-    data = {"gamma": gamma, "learning_rate": learning_rate, "epsilon": eps,
-                         "rewards": rewards, "std_rewards": std_rewards}
-    with open("%s_DQN_random_lr_%s_gamma_%s_clip_%s_seed%s_eval.json" % (environment, learning_rate, gamma, eps, seed),
-              'w+') as f:
-        json.dump(data, f)
-    return rewards, std_rewards
 
-n_configs = 100
+        mean_reward = np.mean(rewards[-100:])
+
+        with tune.checkpoint_dir(i) as checkpoint_dir:
+                path = os.path.join(checkpoint_dir, "checkpoint")
+                model.save(path)
+
+        # TODO: Report a mean or best result
+        tune.report(mean_reward=mean_reward)
+
+
+scheduler = PopulationBasedTraining(
+        time_attr="training_iteration",
+        perturbation_interval=10,
+        hyperparam_mutations={
+            # TODO: Should this be same as the search space?
+            "lr": tune.uniform(lower=math.pow(10, -6), upper=math.pow(10, -2)),
+            "gammas": tune.uniform(lower=0.8, upper=1.0),
+            "epsilons": tune.uniform(0.05, 0.3)
+        },
+    )
+
 seed = 67890
 #Set numpy random seed
 np.random.seed(seed)
-
-learning_rates = np.power(10, np.random.uniform(low=-6, high=-2, size=n_configs))
-gammas = np.random.uniform(low=0.8, high=1, size=n_configs)
-epsilons = np.random.uniform(low=0.05, high=0.3, size=n_configs)
 
 # use tune for hyperparameter selection
 search_space = {
@@ -118,4 +81,52 @@ search_space = {
     "epsilons": tune.uniform(0.05, 0.3)
 }
 
-r, std_r = run_dqn(config=search_space)
+# set `address=None` to train on laptop
+ray.init(address=None)
+
+# use local_mode to run in one process (enables debugging in IDE)
+# ray.init(address=None,local_mode=True)
+
+# TODO: For syncing on cluster see: https://docs.ray.io/en/latest/tune/tutorials/tune-checkpoints.html
+sync_config = tune.SyncConfig()
+
+# r, std_r = run_dqn(config=search_space)
+analysis = tune.run(
+    run_dqn,
+    name="dqn-pbt-tune",
+    scheduler=scheduler,
+    verbose=False,
+    metric="mean_reward",
+    mode="max",
+    num_samples=10,
+
+    # a directory where results are stored before being
+    # sync'd to head node/cloud storage
+    local_dir="tmp/pbtTune",
+
+    # sync our checkpoints via rsync
+    # you don't have to pass an empty sync config - but we
+    # do it here for clarity and comparison
+    sync_config=sync_config,
+
+    # we'll keep the best five checkpoints at all times
+    # checkpoints (by AUC score, reported by the trainable, descending)
+    checkpoint_score_attr="mean_reward",
+    keep_checkpoints_num=5,
+
+    # a very useful trick! this will resume from the last run specified by
+    # sync_config (if one exists), otherwise it will start a new tuning run
+    resume="AUTO",
+    
+    config=search_space,
+    )
+
+print("Best hyperparameters found were: ", analysis.best_config)
+
+
+# best_trial = analysis.best_trial  # Get best trial
+# best_config = analysis.best_config  # Get best trial's hyperparameters
+# best_logdir = analysis.best_logdir  # Get best trial's logdir
+# best_checkpoint = analysis.best_checkpoint  # Get best trial's best checkpoint
+# best_result = analysis.best_result  # Get best trial's last results
+# best_result_df = analysis.best_result_df  # Get best result as pandas dataframe
